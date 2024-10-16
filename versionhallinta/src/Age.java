753c43b22e1dd2e0a4c93b5c26f05eb995c91a62
AinoVakkilainen <ainoeveliina.vakkilainen@gmail.com>
2024-10-16 14:17:45 +0300

    updated versionhallinta

--- versionhallinta/src/Age.java
+++ versionhallinta/src/Age.java
@@ -16,38 +16,6 @@ public class Age {
                 System.out.println(" & kevytmoottoripyörää");
                 }
             }
-
-            else if (ika == 18) {
-                System.out.println("Onneksi olkoon täysi-ikäisyydestä! Voit nyt hankkia ajokortin!");
-            }
-
-            else if (ika == 100) {
-                System.out.println("***~~~~~***\nDude you old man\n***~~~~~***");
-            }
-
-            else if (ika >= 58 && ika < 65) {
-                System.out.println("Eikö työnteko maita? Have you tried *varhaiseläke*?");
-            }
-
-            else if (ika >= 65) {
-               if (ika == 65) {
-                    System.out.println("Hyvejä eläkettä");
-                }
-                else {
-                    System.out.println("Olet eläkeläinen");
-                }
-            }
- 
-            else {
-                System.out.println("Olet aikuinen");
-            if (ika >= 20 && ika <= 60 && ika % 10 == 0){
-                    System.out.println("Onneksi olkoon tasaluvusta!");
-                }
-            if (ika >= 40 && ika <= 50){
-                System.out.println("Onnea keski-iän kriisistä");
-            }
-
-           }
-        
+       
     }
 }
